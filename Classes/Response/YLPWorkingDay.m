8af87efb09d875735a42f6a4fe37adfc7155e568
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2018-10-17 18:05:49 +0300

    add convering from Yelp time format to NSTimeInterval

--- Classes/Response/YLPWorkingDay.m
+++ Classes/Response/YLPWorkingDay.m
@@ -13,10 +13,19 @@
     if (self = [super init]) {
         _overnight = [((NSString *)dayDict[@"is_overnight"]) boolValue];
         _dayNumber = [((NSString *)dayDict[@"day"]) integerValue];
-        _startTime = [dayDict[@"start"] doubleValue];
-        _endTime = [dayDict[@"end"] doubleValue];
+        _startTime = [self timeIntervalFromHoursAndMinutes:[dayDict[@"start"] integerValue]];
+        _endTime = [self timeIntervalFromHoursAndMinutes:[dayDict[@"end"] integerValue]];
     }
     return self;
+}
+
+- (NSTimeInterval)timeIntervalFromHoursAndMinutes:(NSInteger)hoursAndMinutes {
+    NSInteger hours = (hoursAndMinutes / 100) % 100;
+    NSInteger minutes = hoursAndMinutes % 100;
+    NSInteger secondsInOneHour = 3600;
+    NSInteger secondsInOneMinute = 60;
+    
+    return hours * secondsInOneHour + minutes * secondsInOneMinute;
 }
 
 
